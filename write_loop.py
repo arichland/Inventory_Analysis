a9ce004a4d1d1c653082488142bfe3f434e49c3d
A.Richland <r.a.richland@gmail.com>
2020-10-26 11:57:16 -0400

    Created default values for parameters sheets that are generate when sheet is created.
    Formatted headers, columns, and percentages in parameter sheet.

---
+++ write_loop.py
@@ -0,0 +1,53 @@
+_author_ = 'arichland'
+
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+import pymysql.cursors
+from pydict import sql_dict
+import pandas as pd
+
+
+
+
+pd_sum = pd.DataFrame.sum
+user = sql_dict.get('user')
+password = sql_dict.get('password')
+host = sql_dict.get('host')
+database = sql_dict.get('database')
+charset = sql_dict.get('charset')
+
+con = pymysql.connect(user=user,
+                       password=password,
+                       host=host,
+                       database=database,
+                       charset=charset)
+
+filename = "Inventory Analysis.xlsx"
+wb = Workbook()
+ws = wb.active
+
+try:
+    with con.cursor() as cur:
+        sum = 'SELECT Concat, Material, Location, Mat_Descr, Location_Descr FROM rtcaws01.tbl_Analysis;'
+        cur.execute(sum)
+        #rows = cur.fetchall()
+        cols = cur.description
+        col_name = [col[0] for col in cols]
+        data = [dict(zip(col_name, row))
+                for row in cur.fetchall()]
+        df = pd.DataFrame(data)
+        df["Sum"] = df.sum(axis=1)
+        rows = dataframe_to_rows(df, index=True, header=True)
+        for r_idx, row in enumerate(rows, 1):
+            for c_idx, value in enumerate(row, 1):
+                ws.cell(row=2+r_idx, column=c_idx, value=value)
+        ws.delete_cols(1)
+        ws.delete_rows(4)
+        ws.freeze_panes = "B4"
+        print("Dataset:")
+        print(df)
+finally:
+    con.commit()
+    cur.close()
+    con.close()
+wb.save(filename=filename)
