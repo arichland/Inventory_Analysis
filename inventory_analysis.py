47b6d657339fe76f7b2a031428ac8ada61cd47c4
A.Richland <r.a.richland@gmail.com>
2020-10-27 21:15:04 -0400

    Cleaned up and removed unnecessary lines. - removed sql qry for months. - no longer loop qry with month as criteria - import qry results directly into pandas df instead of putting results into a dict or list - removed variables that were no longer required because of streamlined sql qry or dataframe - created pivot table for df that pivots date rows to columns
    Signed-off-by: A.Richland <r.a.richland@gmail.com>

--- inventory_analysis.py
+++ inventory_analysis.py
@@ -1,10 +1,12 @@
 _author_ = 'arichland'
 
-from openpyxl import load_workbook
+import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 import pymysql.cursors
 from pydict import sql_dict
 import pandas as pd
+import numpy as np
+
 
 # SQL Fields
 user = sql_dict.get('user')
@@ -18,71 +20,41 @@ con = pymysql.connect(user=user,
                        database=database,
                        charset=charset)
 
-# Excel Fields
+# Workbook Fields
 filename = "Inventory Analysis.xlsx"
-wb = load_workbook(filename=filename)
+wb = openpyxl.load_workbook(filename=filename)
 wb.active = wb.sheetnames.index("Analysis")
 ws = wb.active
+df_rows = dataframe_to_rows
 
 
+
+# Get monthly inventory reports
 try:
     with con.cursor() as cur:
-        sum = 'SELECT Concat, Material, Location, Mat_Descr, Location_Descr FROM rtcaws01.tbl_Analysis;'
-        cur.execute(sum)
-        #rows = cur.fetchall()
-        cols = cur.description
-        col_name = [col[0] for col in cols]
-        data = [dict(zip(col_name, row))
-                for row in cur.fetchall()]
-        df = pd.DataFrame(data)
-        df["Sum"] = df.sum(axis=1)
-        rows = dataframe_to_rows(df, index=True, header=True)
-        for r_idx, row in enumerate(rows, 1):
-            for c_idx, value in enumerate(row, 1):
-                ws.cell(row=2+r_idx, column=c_idx, value=value)
-        ws.delete_cols(1)
-        ws.delete_rows(4)
-        ws.freeze_panes = "B4"
-        print("Dataset:")
-        print(df)
+        qry_rpt_inven = "SELECT sku, location, abc_class, dates, qty FROM pyInven_Report;"
+
+        # SQL query to pandas dataframe
+        read_df = pd.read_sql(qry_rpt_inven, con)
 finally:
-    con.commit()
-    cur.close()
-    con.close()
+        con.commit()
+        cur.close()
+        con.close()
 
 
+df = pd.DataFrame(read_df)
+#print(df)
 
-
-# Get inventory level by month
-
-try:
-    with con.cursor() as cur:
-        qry_rpt_months = "SELECT distinct date_format(dates, '%Y-%m-1') as Month FROM pyInven_Report;"
-        cur.execute(qry_rpt_months)
-
-
-
-finally:
-    con.commit()
-    cur.close()
-    con.close()
-
-
-
-
-# Assign sheets to variables
-#params = wb["Parameters"]
-#analysis = wb["Analysis"]
-#inven_hist = wb["Inventory History"]
-#cur_inven = wb["Current Inventory"]
-#sales_hist = wb["Sales History"]
-#forecast_hist = wb["Forecast History"]
-#inven_turns = wb["Inventory Turns"]
-#segmen = wb["Segmentation"]
-#segmen_calc = wb["Segmentation Calculation"]
-#material = wb["Material Data"]
-#skus = wb["SKU Data"]
-#locale = wb["Location Data"]
-#conversion = wb["Conversion"]
-
+pivotTableDF = df.filter(items=['sku', 'location', 'qty'])
+pivotTableDF = pd.pivot_table(df,
+                              index='sku',
+                              columns='dates',
+                              values='qty',
+                              aggfunc=np.sum,
+                              margins=True)
+pivotTableDF.sort_values(by=['All'],
+                         inplace=True,
+                         ascending=False)
+print(pivotTableDF)
+# Save to workbook
 wb.save(filename=filename)
